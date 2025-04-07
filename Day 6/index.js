fcd9f86e9f10051991ce625642d1d3f450e5ada9
madhav8452 <madhavrathod019@gmail.com>
2025-04-07 17:36:07 +0530

    Add_Middleware

---
+++ Day 6/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const port = 3000
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended : true}))
+
+const middle = (req,res,next)=>{
+      req.body.task = 'Madhav';
+    next();
+}
+
+let task = []
+
+app.get('/', (req, res)=>{
+    res.render('index', {task})
+})
+
+app.post('/addTask', middle, (req, res)=>{
+    req.body.id = task.length + 1
+    task.push(req.body)
+    res.redirect('/')
+})
+
+app.get('/deleteTask', (req, res)=>{
+    let newTask = task.filter((item)=>item.id != req.query.id)
+    task = newTask
+    res.redirect('/')
+})
+
+app.get('/editTask/:id', (req, res)=>{
+    let singleTask = task.find((item)=>item.id == req.params.id)
+    res.render('edit', {singleTask})
+})
+
+app.post('/updateTask', (req, res)=>{
+    task.map((item)=>{
+        if(item.id == req.body.id){
+            item.task = req.body.task
+        }
+        else{
+            item
+        }
+    })
+    res.redirect('/')
+})
+
+app.listen(port, (error)=>{
+    error ? console.log(error) : console.log(`Started server on port: ${port}`)
+})
